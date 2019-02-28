1300d9f5207b4560578af95ef20a2fb7b675f50b
hongyongbo <hongyongbo@krspace.cn>
2019-02-28 10:47:27 +0800

    docs: 资料整理

--- url.js
+++
@@ -1,8 +0,0 @@
-let url = require('url');
-let u = 'http://member.krspace.cn:8080/abc/index.html?a=1&b=2#hash';
-
-// 可以将查询字符串转化成对象
-let urlObj = url.parse(u);
-// let urlObj = url.parse(u,true);
-console.log(urlObj.query); // 查询字符串
-console.log(urlObj.pathname); //路径
